ca01fa4bfeb271c530b2434769f71905886b056e
Benedict <bmsteve96@gmail.com>
2021-04-03 21:32:09 +0300

    added sass

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Home.module.sass"
+
+const about = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Welcome to about page</h1>
+    </div>
+  )
+}
+
+export default about
