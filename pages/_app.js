9c8b7412528d93c9a3ef3c83cf9903c46059ec56
Benedict <bmsteve96@gmail.com>
2021-04-12 16:11:42 +0300

    added register and login pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,44 +1,23 @@
-import React, { lazy } from "react"
+import React from "react"
+import { useRouter } from "next/router"
 import "../styles/globals.sass"
 import Layout from "../components/layout/Layout"
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+  const router = useRouter()
+
+  if (
+    router.pathname.startsWith("/register") ||
+    router.pathname.startsWith("/login")
+  ) {
+    return <Component {...pageProps} />
+  } else {
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    )
+  }
 }
 
 export default MyApp
-
-
-// /pages/_app_.js
-// import React from 'react'
-// import App from 'next/app'
-// import SiteLayout from '../components/SiteLayout'
-// import AccountSettingsLayout from '../components/AccountSettingsLayout'
-
-// class MyApp extends App {
-//   render() {
-//     const { Component, pageProps, router } = this.props
-
-//     if (router.pathname.startsWith('/account-settings/')) {
-//       return (
-//         <SiteLayout>
-//           <AccountSettingsLayout>
-//             <Component {...pageProps}></Component>
-//           </AccountSettingsLayout>
-//         </SiteLayout>
-//       )
-//     }
-
-//     return (
-//       <SiteLayout>
-//         <Component {...pageProps}></Component>
-//       </SiteLayout>
-//     )
-//   }
-// }
-
-// export default MyApp
---
+++ pages/register.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const register = () => {
+  return (
+    <div>
+      <h2>Register Page</h2>
+    </div>
+  )
+}
+
+export default register
