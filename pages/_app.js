a3069221930dc65d2823f3ba1cdb8a71e3e010bc
Benedict <bmsteve96@gmail.com>
2021-04-12 15:48:52 +0300

    created redux store

--- pages/_app.js
+++ pages/_app.js
@@ -11,3 +11,34 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+
+// /pages/_app_.js
+// import React from 'react'
+// import App from 'next/app'
+// import SiteLayout from '../components/SiteLayout'
+// import AccountSettingsLayout from '../components/AccountSettingsLayout'
+
+// class MyApp extends App {
+//   render() {
+//     const { Component, pageProps, router } = this.props
+
+//     if (router.pathname.startsWith('/account-settings/')) {
+//       return (
+//         <SiteLayout>
+//           <AccountSettingsLayout>
+//             <Component {...pageProps}></Component>
+//           </AccountSettingsLayout>
+//         </SiteLayout>
+//       )
+//     }
+
+//     return (
+//       <SiteLayout>
+//         <Component {...pageProps}></Component>
+//       </SiteLayout>
+//     )
+//   }
+// }
+
+// export default MyApp
