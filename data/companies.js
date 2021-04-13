2bf36e8ace58309889adf205a2ec4676e9fbfee2
Benedict <bmsteve96@gmail.com>
2021-04-13 19:43:29 +0300

    added dummy companies data

---
+++ data/companies.js
@@ -0,0 +1,40 @@
+let companies = [
+  {
+    id: 1,
+    logo: "logo1.png",
+    name: "Company Name",
+    jobs: 18,
+  },
+  {
+    id: 2,
+    logo: "logo2.png",
+    name: "coworker",
+    jobs: 1,
+  },
+  {
+    id: 3,
+    logo: "logo1.png",
+    name: "Company Name",
+    jobs: 19,
+  },
+  {
+    id: 4,
+    logo: "logo2.png",
+    name: "coworker",
+    jobs: 10,
+  },
+  {
+    id: 5,
+    logo: "logo1.png",
+    name: "Company Name",
+    jobs: 3,
+  },
+  {
+    id: 6,
+    logo: "logo2.png",
+    name: "coworker",
+    jobs: 4,
+  },
+]
+
+export default companies
