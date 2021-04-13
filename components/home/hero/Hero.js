cc07b9880a3ba47423849a31cef9eb804bcb325c
Benedict <bmsteve96@gmail.com>
2021-04-13 11:06:43 +0300

    added responsive home hero UI

---
+++ components/home/hero/Hero.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react"
+import { useRouter } from "next/router"
+import styles from "./hero.module.sass"
+
+const Hero = () => {
+  const router = useRouter()
+  const [search, setSearch] = useState({
+    keyword: "",
+    location: "",
+  })
+  const { keyword, location } = search
+  const handleChange = (e) => {
+    let { name, value } = e.target
+    setSearch({ ...search, [name]: value })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (keyword.trim().length > 0 && location.trim().length > 0)
+      router.push(`/jobs?keyword=${keyword}&location=${location}`)
+    if (keyword.trim().length > 0 && location.trim().length == 0)
+      router.push(`/jobs?keyword=${keyword}`)
+    if (keyword.trim().length == 0 && location.trim().length > 0)
+      router.push(`/jobs?location=${location}`)
+    setSearch({
+      keyword: "",
+      location: "",
+    })
+  }
+  return (
+    <div className={styles.hero}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <h1>Main Headline that convinces customer.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Potenti
+            pretium adipiscing tortor convallis aliquet cursus. Sed consequat
+            sed id in nullam lectus parturient. Ullamcorper arcu gravida et
+            aliquam ornare quam.
+          </p>
+          <div className={styles.search__container}>
+            <form
+              onSubmit={(e) => handleSubmit(e)}
+              className={`search__form ${styles.search__form}`}
+            >
+              <input
+                type="text"
+                placeholder="Type to search..."
+                onChange={(e) => handleChange(e)}
+                name="keyword"
+              />
+              <input
+                type="text"
+                placeholder="Location"
+                onChange={(e) => handleChange(e)}
+                name="location"
+              />
+              <button className={`btn btn-primary ${styles.btn}`}>
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import Hero from "../components/hero/Hero"
+import Hero from "../components/home/hero/Hero"
 
 export default function Home() {
   return (
