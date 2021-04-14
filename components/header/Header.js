adc25102a4394086e4cb9eba0f9a79fd9c15bda7
Innocent Suta <sutainnocent@gmail.com>
2021-04-14 14:22:56 +0300

    Header - Not Finished yet.

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,34 +1,45 @@
 import Link from "next/link"
 import { AiOutlineSearch } from "react-icons/ai"
+import styles from "./Header.module.sass"
 
 const Header = () => {
   return (
-    <header>
-      <div className="container">
-        <h1>
-          <Link href="/">Logo</Link>
+    <header className={styles.header}>
+      
+      <div className={`${styles.main_nav} ${styles.container}`}>
+
+        <h1 className={styles.logo}>
+          <Link href="/">
+            <a  href="/"><span>Job</span>Portal.</a>
+            </Link>
         </h1>
-        <nav>
+
+        <nav className={styles.navbar_menu}>
           <ul>
-            <li>
-              <Link href="/jobs">Jobs</Link>
+            <li className={`${styles.nav_item} ${styles.active}`}>
+              <Link href="/jobs"><a href="/jobs" className="nav_link">Jobs</a></Link>
             </li>
-            <li>
-              <Link href="/about">About</Link>
+            <li className={styles.nav_item}>
+              <Link href="/about"><a href="/about" className="nav_link">About</a></Link>
             </li>
-            <li>
-              <Link href="/contact">Contact Us</Link>
+            <li className={styles.nav_item}>
+              <Link href="/contact"><a href="/contact" className="nav_link">Contact</a></Link>
             </li>
-            <li>
+            <li className={styles.nav_item}>
               <AiOutlineSearch className="icon" />
             </li>
           </ul>
-        </nav>
-        <nav>
-          <Link href="/login">Login</Link>
-          <Link href="/register" className="btn btn-primary">
-            Join Us
-          </Link>
+        
+        <ul className={styles.navbar_user}>
+          <li>
+            <Link href="/login"><a href="/login" className="nav_link">Login</a></Link>
+          </li>
+          <li>
+             <Link href="/register" className="btn btn-primary">
+             <a href="/register" className="nav_link">Join Us</a>
+             </Link>
+          </li>
+        </ul>
         </nav>
       </div>
     </header>
