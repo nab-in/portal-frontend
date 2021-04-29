c689e9b5ca8fb41c7106fa5a8a4f016ade37afcb
Benedict <bmsteve96@gmail.com>
2021-04-29 16:44:51 +0300

    updated header jsx

--- components/header/Header.js
+++ components/header/Header.js
@@ -2,30 +2,36 @@ import React, { useState } from "react"
 import Link from "next/link"
 import { AiOutlineSearch,AiOutlineMenu } from "react-icons/ai"
 import styles from "./Header.module.sass"
-import { IconContext } from "react-icons/lib"
+// import { IconContext } from "react-icons/lib"
 
 const Header = () => {
 
   const [isOpen, setisOpen] = useState(false);
+  cons [search, setSearch] = useState(false)
 
-  const handleClick = () => {
-    
+  const toggleMenu = () => {
     setisOpen(!isOpen);
+  }
+
+  const toggleSearch = () => {
+    setSearch(!search)
   }
 
   return (
     <header className={styles.header}>
-      <div className={`${styles.main_nav} ${styles.container}`}>
-        <h1 className={styles.logo}>
-          <Link href="/">
-            <a href="/">
-              <span>Job</span>Portal.
-            </a>
-          </Link>
-        </h1>
-
-        <nav className={styles.navbar_menu}>
-          <ul>
+      <div className={styles.container}>
+       <div className={styles.logo__container}>
+          <div className={styles.logo}>
+            <Image
+              src={`/assets/images/logo.png`}
+              alt={`logo`}
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        </div>
+        <nav>
+          <ul className={isOpen? `${styles.menu} ${styles.open}`: `${styles.menu}`}>
             <li className={`${styles.nav_item} ${styles.active}`}>
               <Link href="/jobs">
                 <a href="/jobs" className="nav_link">
@@ -52,14 +58,11 @@ const Header = () => {
                 </a>
               </Link>
             </li>
-            <li className={styles.nav_item}>
-              <AiOutlineSearch className={styles.icon}/>
-            </li>
           </ul>
-          </nav>
-
-          <nav className={styles.navbar_user}>
-          <ul >
+          <button className={styles.toggle__search} onClick={toggleSearch}>
+              <AiOutlineSearch className={styles.icon}/>
+            </button>
+           <ul className={styles.auth__links}>
             <li>
               <Link href="/login">
                 <a href="/login" className="nav_link">
@@ -75,9 +78,27 @@ const Header = () => {
               </Link>
             </li>
           </ul>
-        </nav>
+          </nav>
 
-        <button className={styles.toggle__button} onClick={handleClick}> <AiOutlineMenu />  </button>
+        <div className={styles.burger}>
+        <button className={styles.toggle__button} onClick={handleClick}> <AiOutlineMenu /></button>
+        </div>
+        <div className={search? `${styles.search__container} ${styles.open}`: `${styles.search__container}`}>
+         <form
+              onSubmit={(e) => handleSubmit(e)}
+              className={`search__form ${styles.search__form}`}
+            >
+              <input
+                type="text"
+                placeholder="Type to search..."
+                onChange={(e) => handleChange(e)}
+                name="keyword"
+              />
+              <button className={`btn btn-primary ${styles.btn}`}>
+                Search
+              </button>
+            </form>
+        </div>
   
       </div>
     </header>
