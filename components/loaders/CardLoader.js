fe4d39c12a2a11638583e1e7ea899c0eb9dadb69
Benedict <bmsteve96@gmail.com>
2021-05-17 14:56:31 +0300

    added waves to the card loader

--- components/loaders/CardLoader.js
+++ components/loaders/CardLoader.js
@@ -4,10 +4,16 @@ const CardLoader = () => {
     <article className={`card ${styles.card}`}>
       <div className={styles.logo}></div>
       <div className={styles.details}>
-        <h2></h2>
+        <h2>
+          <span className={styles.loader}></span>
+        </h2>
         <div className={`stars ${styles.stars}`}></div>
-        <p></p>
-        <p></p>
+        <p>
+          <span className={styles.loader}></span>
+        </p>
+        <p>
+          <span className={styles.loader}></span>
+        </p>
       </div>
     </article>
   )
