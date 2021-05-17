f7e03531dcc592e19bb1f2153f819dd5b29c0999
Benedict <bmsteve96@gmail.com>
2021-05-17 17:00:10 +0300

    added companies loader

--- components/home/companies/Companies.js
+++ components/home/companies/Companies.js
@@ -1,31 +1,43 @@
 import React from "react"
 import Link from "next/link"
+import Loader from "../../loaders/CompaniesLoader"
 import styles from "./companies.module.sass"
 import companies from "../../../data/companies"
 
+let loading = true
 const Companies = () => {
   return (
     <section className={styles.companies}>
       <div className={`${styles.container} container`}>
         <h2 className="primary__header">Trusted By</h2>
         <div className={`${styles.showcase}`}>
-          {companies.slice(1, 7).map(({ logo, name, id, jobs }) => (
-            <article key={id}>
-              <Link href={`/companies/${id}`}>
-                <div className={`${styles.logo__container}`}>
-                  <img
-                    src={`/assets/companies/${logo}`}
-                    alt={`${name} logo`}
-                    layout="fill"
-                    objectFit="contain"
-                  />
-                </div>
-              </Link>
-              <Link href={`/companies/${id}/jobs/`}>
-                <a>View {jobs} Jobs</a>
-              </Link>
-            </article>
-          ))}
+          {loading ? (
+            <>
+              <Loader />
+              <Loader />
+              <Loader />
+            </>
+          ) : (
+            <>
+              {companies.slice(1, 7).map(({ logo, name, id, jobs }) => (
+                <article key={id}>
+                  <Link href={`/companies/${id}`}>
+                    <div className={`${styles.logo__container}`}>
+                      <img
+                        src={`/assets/companies/${logo}`}
+                        alt={`${name} logo`}
+                        layout="fill"
+                        objectFit="contain"
+                      />
+                    </div>
+                  </Link>
+                  <Link href={`/companies/${id}/jobs/`}>
+                    <a>View {jobs} Jobs</a>
+                  </Link>
+                </article>
+              ))}
+            </>
+          )}
         </div>
         <div className={styles.link__container}>
           <Link href={`/companies`}>
