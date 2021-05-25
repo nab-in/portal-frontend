37974c4f95c18ffe8256cec1a1593c99e287fe21
Benedict Steven <48621248+bmsteven@users.noreply.github.com>
2021-05-25 14:59:43 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 const withPWA = require("next-pwa")
 const runtimeCaching = require("next-pwa/cache")
 
-module.exports = {
-  pwa: withPWA({
+module.exports = withPWA({
     pwa: {
       dest: "public",
       runtimeCaching,
     },
-  }),
   publicRuntimeConfig: {
     API_URL: "http://107.191.98.130/backend/api",
   },
-}
+})
