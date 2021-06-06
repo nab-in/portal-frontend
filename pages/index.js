a0e99cfc4739d8b6bf090c533319756287045a3d
Benedict <bmsteve96@gmail.com>
2021-06-06 12:54:08 +0300

    called /api/jobs for home and jobs pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import React, { useEffect, useState } from "react"
 import Head from "next/head"
 import Hero from "../components/home/hero/Hero"
 import Jobs from "../components/jobs_template/Jobs"
 import Companies from "../components/home/companies/Companies"
 import { API } from "../components/api"
 
-const Home = () => {
+const Home = ({ jobs }) => {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    if (jobs) setLoading(false)
+  }, [jobs])
   return (
     <div>
       <Head>
@@ -13,11 +18,22 @@ const Home = () => {
       </Head>
       <Hero />
       <main>
-        <Jobs heading="Recent Jobs" />
+        <Jobs heading="Recent Jobs" jobs={jobs} loading={loading} />
         <Companies />
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API}/jobs`)
+  const jobs = await res.json()
+
+  return {
+    props: {
+      jobs,
+    },
+  }
 }
 
 export default Home
--- pages/jobs.js
+++ pages/jobs.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Hero from "../components/filter_hero/Hero"
 import Jobs from "../components/jobs_template/Jobs"
 import categories from "../data/categories"
+import { API } from "../components/api"
 
-const jobs = () => {
+const jobs = ({ jobs }) => {
   const [search, setSearch] = useState({
     keyword: "",
     location: "",
     categories: [],
   })
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    if (jobs) setLoading(false)
+  }, [jobs])
   return (
     <div className="jobs">
       <Hero
@@ -24,10 +29,23 @@ const jobs = () => {
           setSearch={setSearch}
           heading="Recent Jobs"
           page="jobs"
+          jobs={jobs}
+          loading={loading}
         />
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API}/jobs`)
+  const jobs = await res.json()
+
+  return {
+    props: {
+      jobs,
+    },
+  }
 }
 
 export default jobs
