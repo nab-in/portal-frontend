bdf47f877cbac469c065c500fb0baf4a5e4a8778
Benedict <bmsteve96@gmail.com>
2021-08-29 14:07:29 +0300

    update footer change social media from texts to icons

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,151 +1,22 @@
-import styles from "./Footer.module.sass"
+import React from "react"
+import styles from "./footer.module.sass"
 import Link from "next/link"
+import Social from "./Social"
 import Input from "../inputs/Input"
 import FormButton from "../buttons/FormButton"
 import Logo from "../BgLogo"
 
 const Footer = () => {
-  const handleChange = (e) => {}
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  }
-
   return (
-    <footer className={styles.footer_container}>
-      <div className={styles.footer_left}>
-        <h1 className={styles.logo}>
-          <Link href="/">
-            <a>
-              <Logo height={25} />
-            </a>
-          </Link>
-        </h1>
-        <p className={styles.footer_call_to_action}>
-          Sign Up today to start applying for different jobs and posting jobs
-          for different professions
-          <Link href="/register">
-            <a className={styles.footer_links}>Register</a>
-          </Link>
-        </p>
-        <p className={styles.footer_call_to_action}>
-          Already Registered?
-          <Link href="/login">
-            <a className={styles.footer_links}>Login</a>
-          </Link>
-        </p>
-
-        <div>
-          <p className={styles.footer_call_to_action}>
-            Subscribe to start receiving notifications when new jobs are posted{" "}
-          </p>
-
-          <form onSubmit={(e) => handleSubmit(e)}>
-            <Input
-              type="text"
-              name="username"
-              handleChange={handleChange}
-              id="name"
-              title="Name:"
-              inputClass="bg_input"
-              placeholder="Enter your full name"
-            />
-            <Input
-              type="email"
-              name="email"
-              handleChange={handleChange}
-              id="email"
-              title="Email address:"
-              inputClass="bg_input"
-              placeholder="Enter your email address"
-            />
-            <FormButton
-              text="Subscribe"
-              btnClass="btn-primary"
-              btnGroupClass="btns"
-            />
-          </form>
+    <footer className={styles.footer}>
+      <section className={styles.footer__right}>
+        <div className={styles.footer__left__container}></div>
+      </section>
+      <section className={styles.footer__left}>
+        <div className={styles.footer__left__container}>
+          <Social />
         </div>
-
-        <div className={styles.footer_copyright}>
-          <p>
-            &#169; 2021 -{" "}
-            <b>
-              <span>Job</span>Portal.
-            </b>
-          </p>
-        </div>
-      </div>
-
-      <div className={`${styles.footer_right} ${styles.flexgrid}`}>
-        <div className={`${styles.f_browse} ${styles.flexcol}`}>
-          <h3>Browse</h3>
-          <ul>
-            <li>
-              <Link href="/jobs">
-                <a>Jobs</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/companies">
-                <a>Companies</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className={`${styles.f_jobs} ${styles.flexcol}`}>
-          <h3>About</h3>
-          <ul>
-            <li>
-              <Link href="/about">About us</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact us</Link>
-            </li>
-            <li>
-              <Link href="/help">Help</Link>
-            </li>
-            <li>
-              <Link href="/terms">
-                <a>Terms of Use</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className={`${styles.f_connect} ${styles.flexcol}`}>
-          <h3>Connect</h3>
-          <h1 className={styles.logo2}>
-            <Link href="/">
-              <a>
-                <Logo height={20} />
-              </a>
-            </Link>
-          </h1>
-          <ul>
-            <li>
-              <a href="/">Facebook</a>
-            </li>
-            <li>
-              <a href="/">Twitter</a>
-            </li>
-            <li>
-              <a href="/">LinkedIn</a>
-            </li>
-            <li>
-              <a href="/">Instagram</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className={`${styles.footer_copyright} ${styles.mobile}`}>
-        <p>
-          &#169; 2021 -{" "}
-          <b>
-            <span>Job</span>Portal.
-          </b>
-        </p>
-      </div>
+      </section>
     </footer>
   )
 }
---
+++ components/footer/Social.js
@@ -0,0 +1,48 @@
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaInstagram,
+} from "react-icons/fa"
+import Link from "next/link"
+import Logo from "../BgLogo"
+import styles from "./social.module.sass"
+
+const Social = () => {
+  return (
+    <article className={styles.social}>
+      <h3>Connect</h3>
+      <Link href="/">
+        <a className={styles.logo}>
+          <Logo height={25} />
+        </a>
+      </Link>
+      <div className={styles.social__showcase}>
+        <ul>
+          <li>
+            <a href="http://facebook.com" target="_blank">
+              <FaFacebookF className={styles.social__icon} />
+            </a>
+          </li>
+          <li>
+            <a href="http://twitter.com" target="_blank">
+              <FaTwitter className={styles.social__icon} />
+            </a>
+          </li>
+          <li>
+            <a href="http://linkedin.com" target="_blank">
+              <FaLinkedinIn className={styles.social__icon} />
+            </a>
+          </li>
+          <li>
+            <a href="http://instagram.com" target="_blank">
+              <FaInstagram className={styles.social__icon} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </article>
+  )
+}
+
+export default Social
