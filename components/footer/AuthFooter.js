523bd4be6792ce7461f432fc5904e35e6a234ff3
Benedict <bmsteve96@gmail.com>
2021-08-29 22:13:14 +0300

    finish authfooter UI

--- components/footer/AuthFooter.js
+++ components/footer/AuthFooter.js
@@ -1,7 +1,99 @@
-import React from "react"
+import styles from "./authfooter.module.sass"
+import Link from "next/link"
+import { useAuthState } from "../../context/auth"
+import { DASH } from "../api"
+import Social from "./Social"
 
 const AuthFooter = () => {
-  return <div>Footer</div>
+  const { roles } = useAuthState()
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+  }
+  console.log(roles)
+  return (
+    <footer className={styles.footer}>
+      <section className={styles.footer__container}>
+        <article className={styles.footer__links}>
+          <h3>Browse</h3>
+          <div className={styles.showcase}>
+            <ul>
+              {roles?.length > 0 &&
+                roles.map(({ id, name }) => (
+                  <li key={id}>
+                    <a href={`${DASH}/select_identity?role=${id}`}>
+                      {capitalizeFirstLetter(name)} Dashboard
+                    </a>
+                  </li>
+                ))}
+              <li>
+                <Link href="/jobs">
+                  <a>Jobs</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/companies">
+                  <a>Companies</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a>Profile</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a>Applied Jobs</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <a>Saved Jobs</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </article>
+        <article className={styles.footer__links}>
+          <h3>About</h3>
+          <div className={styles.showcase}>
+            <ul>
+              <li>
+                <Link href="/about">About us</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact us</Link>
+              </li>
+              <li>
+                <Link href="/help">Help</Link>
+              </li>
+              <li>
+                <Link href="/terms">
+                  <a>Terms of Use</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </article>
+        <article className={styles.social}>
+          <Social />
+        </article>
+        <div className={`${styles.footer_copyright} ${styles.mobile}`}>
+          <p>
+            &#169; 2021 -&nbsp;
+            <span>Job</span>Portal. Designed and Developed by&nbsp;
+            <a href="http://github.com/nab-in" target="_blank">
+              We Code Together
+            </a>
+          </p>
+        </div>
+      </section>
+    </footer>
+  )
 }
 
 export default AuthFooter
